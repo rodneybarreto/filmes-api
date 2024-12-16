96fecc7109295f2cd6f76af0591123f8e399919d
rodneybarreto <rwbarreto@gmail.com>
2024-12-16 17:32:14 -0300

    Chore: renomeia class de configuração

--- src/main/java/br/uece/eescomdevops/filmesapi/base/configuration/JacksonConfig.java
+++ src/main/java/br/uece/eescomdevops/filmesapi/base/configuration/JacksonConfig.java
@@ -5,5 +5,5 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
-public class JascksonConfig {
+public class JacksonConfig {
 }
