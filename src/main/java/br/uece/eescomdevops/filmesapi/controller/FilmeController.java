c7e5729e480c4c49aad848ac96c8960ffa1a6f52
rodneybarreto <rwbarreto@gmail.com>
2024-12-23 18:54:34 -0300

    refactory: implementa FilmeService

--- src/main/java/br/uece/eescomdevops/filmesapi/controller/FilmeController.java
+++ src/main/java/br/uece/eescomdevops/filmesapi/controller/FilmeController.java
@@ -2,16 +2,13 @@ package br.uece.eescomdevops.filmesapi.controller;
 
 import br.uece.eescomdevops.filmesapi.domain.dto.FilmeReq;
 import br.uece.eescomdevops.filmesapi.domain.dto.FilmeRes;
-import br.uece.eescomdevops.filmesapi.domain.entity.Filme;
-import br.uece.eescomdevops.filmesapi.repository.FilmeRepository;
-import jakarta.persistence.EntityNotFoundException;
+import br.uece.eescomdevops.filmesapi.service.FilmeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -25,46 +22,41 @@ public class FilmeController {
 
     protected static final String RESOURCE = "/v1/filmes";
 
-    private final FilmeRepository filmeRepository;
+    private final FilmeService filmeService;
 
     @Autowired
-    public FilmeController(FilmeRepository filmeRepository) {
-        this.filmeRepository = filmeRepository;
+    public FilmeController(FilmeService filmeService) {
+        this.filmeService = filmeService;
     }
 
-    @Transactional
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> create(@Valid @RequestBody FilmeReq filmeReq, UriComponentsBuilder uriComponentsBuilder) {
-        Filme filme = filmeRepository.save(new Filme(filmeReq));
-        URI uri = uriComponentsBuilder.path(RESOURCE + "/{id}").buildAndExpand(filme.getId()).toUri();
+        Long filmeId = filmeService.create(filmeReq);
+        URI uri = uriComponentsBuilder.path(RESOURCE + "/{id}").buildAndExpand(filmeId).toUri();
         return ResponseEntity.created(uri).build();
     }
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Page<FilmeRes>> findAll(@PageableDefault(sort = {"titulo"}) Pageable pageable) {
-        Page<Filme> filmesPage = filmeRepository.findAll(pageable);
-        return ResponseEntity.ok(filmesPage.map(FilmeRes::new));
+        Page<FilmeRes> filmeResPage = filmeService.findAll(pageable);
+        return ResponseEntity.ok(filmeResPage);
     }
 
     @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<FilmeRes> findById(@PathVariable Long id) {
-        Filme filme = filmeRepository.getReferenceById(id);
-        return ResponseEntity.ok(new FilmeRes(filme));
+        FilmeRes filmeRes = filmeService.findById(id);
+        return ResponseEntity.ok(filmeRes);
     }
 
-    @Transactional
     @PutMapping(value = "/{id}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<FilmeReq> update(@PathVariable Long id, @Valid @RequestBody FilmeReq filmeReq) {
-        Filme filme = filmeRepository.getReferenceById(id);
-        filme.update(filmeReq);
+        filmeService.update(id, filmeReq);
         return ResponseEntity.ok(filmeReq);
     }
 
-    @Transactional
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        Filme filme = filmeRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        filmeRepository.delete(filme);
+        filmeService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
