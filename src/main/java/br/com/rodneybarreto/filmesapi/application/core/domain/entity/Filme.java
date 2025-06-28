f00fe24b4a6115b2fb62d55f0f73e705784b767b
rodneybarreto <rwbarreto@gmail.com>
2025-06-28 11:37:31 -0300

    Usando lombok no domain

--- src/main/java/br/com/rodneybarreto/filmesapi/application/core/domain/entity/Filme.java
+++ src/main/java/br/com/rodneybarreto/filmesapi/application/core/domain/entity/Filme.java
@@ -1,5 +1,14 @@
 package br.com.rodneybarreto.filmesapi.application.core.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Filme {
 
     private Long id;
@@ -9,46 +18,5 @@ public class Filme {
     private String sinopse;
 
     private Integer anoLancamento;
-
-    public Filme() {}
-
-    public Filme(Long id, String titulo, String sinopse, Integer anoLancamento) {
-        this.id = id;
-        this.titulo = titulo;
-        this.sinopse = sinopse;
-        this.anoLancamento = anoLancamento;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getSinopse() {
-        return sinopse;
-    }
-
-    public void setSinopse(String sinopse) {
-        this.sinopse = sinopse;
-    }
-
-    public Integer getAnoLancamento() {
-        return anoLancamento;
-    }
-
-    public void setAnoLancamento(Integer anoLancamento) {
-        this.anoLancamento = anoLancamento;
-    }
 
 }
