f895e12a20e4e552eb7243d08441d6dfb8a5a8f5
rodneybarreto <rwbarreto@gmail.com>
2026-01-30 17:54:33 -0300

    chore: remove classe não utilizada

--- src/main/java/br/com/rodneybarreto/moviesapi/infrastructure/configuration/JacksonConfig.java
+++
@@ -1,9 +0,0 @@
-//package br.com.rodneybarreto.filmesapi.infrastructure.config.jackson;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.web.config.EnableSpringDataWebSupport;
-//
-//@Configuration
-//@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
-//public class JacksonConfig {
-//}
